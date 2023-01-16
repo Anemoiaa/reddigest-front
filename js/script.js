0aa28393fc17319a04ea421ac41dd17fe10f9248
anemoiaa <georgiypetroff@gmail.com>
2023-01-16 18:21:35 +0400

    feat: implement logo scroll animation

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const header = document.querySelector('.header');
     const headerDate = document.querySelector('.header__date');
     const headerIcons = document.querySelectorAll('.header__icon');
+    const afterHeader = document.querySelector('.after-header');
+    const logo = document.querySelector('.logo img');
 
     window.onscroll = function() {
         headerShrink()
@@ -17,7 +19,9 @@ document.addEventListener('DOMContentLoaded', () => {
             headerIcons.forEach(icon => {
                 icon.style.width = '24px';
                 icon.style.height = '24px';
-            })
+            });
+            afterHeader.style.marginTop = '0px';
+            logo.style.width = '80%'
 
         } else {
             header.style.height = '60px';
@@ -27,6 +31,8 @@ document.addEventListener('DOMContentLoaded', () => {
                 icon.style.width = '25px';
                 icon.style.height = '25px';
             })
+            afterHeader.style.marginTop = '12px';
+            logo.style.width = '100%';
         }
     }
 });
