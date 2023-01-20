a91f43545e25e82d568e0b7c961f9c09d10bf713
anemoiaa <georgiypetroff@gmail.com>
2023-01-20 16:22:11 +0400

    refactor: header

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('DOMContentLoaded', () => {
 
     const header = document.querySelector('.header');
@@ -6,26 +5,28 @@ document.addEventListener('DOMContentLoaded', () => {
     const headerIcons = document.querySelectorAll('.header__icon');
     const afterHeader = document.querySelector('.after-header');
     const logo = document.querySelector('.logo img');
+    const logoDiv = document.querySelector('.logo')
 
-    window.onscroll = function() {
+    window.onscroll = function () {
         headerShrink()
     };
     headerShrink()
 
     function headerShrink() {
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-            header.style.height = '30px';
+            header.style.height = '17px';
             headerDate.style.padding = '4px 45px';
             headerDate.style.fontSize = '18px';
             headerIcons.forEach(icon => {
-                icon.style.width = '24px';
-                icon.style.height = '24px';
+                icon.style.width = '15px';
+                icon.style.height = '15px';
             });
             afterHeader.style.marginTop = '0px';
-            logo.style.width = '80%'
+            logo.style.width = '50%';
+            logoDiv.style.marginLeft = '-5px';
 
         } else {
-            header.style.height = '60px';
+            header.style.height = '37px';
             headerDate.style.padding = '18px 33px';
             headerDate.style.fontSize = '20px';
             headerIcons.forEach(icon => {
@@ -33,7 +34,9 @@ document.addEventListener('DOMContentLoaded', () => {
                 icon.style.height = '25px';
             })
             afterHeader.style.marginTop = '12px';
-            logo.style.width = '100%';
+            logo.style.width = '80%';
+            logoDiv.style.marginLeft = '-9px';
         }
     }
 });
+
