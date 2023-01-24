5a19044af41bf7bf288ea9e7bd95f78f04a043b1
anemoiaa <georgiypetroff@gmail.com>
2023-01-24 18:35:25 +0400

    refactor: minor changes

--- js/scroll.js
+++ js/scroll.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const afterHeader = document.querySelector('.after-header');
     const logo = document.querySelector('.logo img');
     const logoDiv = document.querySelector('.logo');
+    const searchBar = document.querySelector('.search-bar');
 
     window.onscroll = function () {
         afterHeaderShrink()
@@ -14,11 +15,13 @@ document.addEventListener('DOMContentLoaded', () => {
             afterHeader.style.top = '0';
             logo.style.width = '60%';
             logoDiv.style.marginLeft = '-5px';
+            searchBar.style.padding = '2px 15px'
 
         } else {
             afterHeader.style.top = '56px';
             logo.style.width = '80%';
-            logoDiv.style.marginLeft = '-9px';;
+            logoDiv.style.marginLeft = '-9px';
+            searchBar.style.padding = '5px 15px'
         }
     }
 });
