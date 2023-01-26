77efe349422295afc02361876668127e8e767d44
anemoiaa <georgiypetroff@gmail.com>
2023-01-26 14:43:53 +0400

    feat: add burger menu

--- js/scroll.js
+++ js/scroll.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const logo = document.querySelector('.logo img');
     const logoDiv = document.querySelector('.logo');
     const searchBar = document.querySelector('.search-bar');
+    const burgerMenu = document.querySelector('.burger-menu .menu');
+    const burgerMenuBtn = document.querySelector('.burger-menu');
 
     window.onscroll = function () {
         afterHeaderShrink()
@@ -11,18 +13,28 @@ document.addEventListener('DOMContentLoaded', () => {
     afterHeaderShrink()
 
     function afterHeaderShrink() {
-        if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+        if (document.body.scrollTop > 70 || document.documentElement.scrollTop > 70) {
             afterHeader.style.top = '0';
             logo.style.width = '60%';
             logoDiv.style.marginLeft = '-5px';
-            searchBar.style.padding = '2px 15px'
+            searchBar.style.padding = '2px 15px';
+            burgerMenuBtn.style.display = 'block';
 
         } else {
             afterHeader.style.top = '56px';
             logo.style.width = '80%';
             logoDiv.style.marginLeft = '-9px';
-            searchBar.style.padding = '5px 15px'
+            searchBar.style.padding = '5px 15px';
+            burgerMenuBtn.style.display = 'none';
         }
     }
+
+    burgerMenuBtn.addEventListener('click', () => {
+        if(burgerMenu.style.display == 'block') {
+            burgerMenu.style.display = 'none'
+        } else {
+            burgerMenu.style.display = 'block';
+        }
+    })
 });
 
